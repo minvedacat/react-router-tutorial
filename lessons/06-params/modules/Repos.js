85fd2506302c37c1bcb6a8c1f4f1675e1d52fdba
Min Zhao <min@veda.cat>
2016-10-14 20:39:13 +0800

    cool

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Repos</div>
+    return <div>
+      <h2>Repos</h2>
+      <ul>
+        <li><Link to="/repos/reactjs/react-router">Ahahaha</Link></li>
+        <li><Link to="/repos/minvedacat/react-router-tutorial">Heyheyhey</Link></li>
+      </ul>
+    </div>
   }
 })
