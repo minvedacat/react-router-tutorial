f7ee73ff6f7e2caab4c1a99c974edd65d8b6ab83
Min Zhao <min@veda.cat>
2016-10-14 21:09:09 +0800

    typo fix

--- lessons/11-productionish-server/server.js
+++ lessons/11-productionish-server/server.js
@@ -5,7 +5,7 @@ var app = express()
 
 app.use(express.static(__dirname))
 
-app.get('*', function (req, ers) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
